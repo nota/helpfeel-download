7edf2f8a36b4bb0f1281c15bf6d606e2f0b5901e
Isshu Rakusai <rakusai@kamilabo.jp>
2019-09-25 09:25:37 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# helpfeel-download
+
+## Install
+
+`npm i`
+
+## How to use
+
+Run this command:
+
+`PROJECT=xxxx node index.js`
+
+then, all resouces will be saved at `out/` directory.
+
+To confirm it, put the files in any local server.
+For example, you could do like this:
+```
+npm i http-server -g
+http-server out/
+```
---
+++ index.js
@@ -0,0 +1,78 @@
+const fs = require('fs')
+const fetch = require('node-fetch')
+const { JSDOM, VirtualConsole }  = require('jsdom')
+
+const projectName = process.env.PROJECT
+if (!projectName) {
+  console.log('Requires project name. Call: PROJECT=projectName node index.js')
+  process.exit(1)
+}
+console.log('Got projectName', projectName)
+
+const outDir = 'out/'
+
+const convertToAbsolutePath = (href, base) => new URL(href, base).href
+
+const localRelativePath = (href) => {
+  return new URL(href).pathname.substring(1)
+}
+
+const fetchAndSave = async (resouceUrl) => {
+  console.log('fetching...', resouceUrl)
+  const res = await fetch(resouceUrl)
+  const text = await res.text()
+  const filePath = outDir + localRelativePath(resouceUrl)
+  const dirPath = filePath.replace(/\/[^/]+$/, '')
+  fs.mkdirSync(dirPath, { recursive: true })
+  fs.writeFileSync(filePath, text)
+
+  return { text }
+}
+
+const fetchHelpData = async () => {
+  const url = `https://helpfeelapp.com/helpdata/${projectName}.json`
+  await fetchAndSave(url)
+}
+
+const main = async () => {
+  const url = `https://helpfeelapp.com/${projectName}/`
+  console.log('fetching...', url)
+  const res = await fetch(url)
+  const html = await res.text()
+
+  const virtualConsole = new VirtualConsole()
+  const { document } = new JSDOM(html, { virtualConsole }).window
+
+  // collect resources
+  // script tag
+  const scripts = Array.from(document.getElementsByTagName('script'))
+    .filter(script => !!script.src)
+    .map(script => convertToAbsolutePath(script.src, url))
+  // link refl=prefetch
+  const links = Array.from(document.querySelectorAll('link[rel="prefetch"]'))
+    .filter(link => !!link.href)
+    .map(link => convertToAbsolutePath(link.href, url))
+
+  const resourceUrls = [...scripts, ...links]
+
+  const svgImages = []
+  for (const resouceUrl of resourceUrls) {
+    const { text } = await fetchAndSave(resouceUrl)
+    // SVG icons in the script
+    const svgPaths = text.match(/\/img\/[^\"\)]*?\.svg/g)
+    if (svgPaths) {
+      svgImages.push(...svgPaths)
+    }
+  }
+
+  for (const image of svgImages) {
+    const absPath = convertToAbsolutePath(image, url)
+    await fetchAndSave(absPath)
+  }
+
+  fs.writeFileSync(outDir + 'index.html', html)
+
+  await fetchHelpData()
+}
+
+main()
